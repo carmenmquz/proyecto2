6e698e987ae3d98ce96224b9dc596ba6d1118268
Jorge <1833981@alu.murciaeduca.es>
2022-12-12 22:02:16 +0100

    Reemplazado layouts master y añadido partials header y partials footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="parallax">
+    <div id="titulo-andromeda">
+        <a href="#cabecera"><img src="img/marca/titulo.png"></a>
+    </div>
+    <div class="container peque">
+        <p id="correo">info.proyectoandromeda@gmail.com</p>
+        <p id="copyright">&copy; Copyright 2021 Andromeda</p>
+    </div>
+</footer>
