91761bd23c7a674d34eb930bd050e37bf7dd9c0f
carmen <carmenmquz@gmail.com>
2022-12-13 09:27:12 +0100

    correccionVistas

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,8 +8,8 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;600;700;900&display=swap" rel="stylesheet">
 
-        <link rel="shortcut icon" href="img/marca/logo-sencillo.png">
-        <link rel="stylesheet" href="assets/vista-andromeda.css">
+        <link rel="shortcut icon" href="/img/marca/logo-sencillo.png">
+        <link rel="stylesheet" href="/assets/vista-andromeda.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     </head>
     <body>
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="parallax">
     <div id="titulo-andromeda">
-        <a href="#cabecera"><img src="img/marca/titulo.png"></a>
+        <a href="#cabecera"><img src="/img/marca/titulo.png"></a>
     </div>
     <div class="container peque">
         <p id="correo">info.proyectoandromeda@gmail.com</p>
