f8f0a16d8cb178c946ef99202597ff081f433ea3
carmen <carmenmquz@gmail.com>
2022-12-14 21:40:30 +0100

    Modificadas las vistas de index y show. Arreglado array ServicioController

--- resources/views/servicios/index.blade.php
+++ resources/views/servicios/index.blade.php
@@ -11,10 +11,21 @@
            @endphp
        <div class="col-xs-6 col-sm-4 col-md-3 text-center">
 
-           <a href="{{ url('/servicios/show/' . $id ) }}">
+           <a href="{{ url('/servicios/show/' . ($id - 1)) }}">
+            <br>
+            <br>
+            <br>
                <img src="https://picsum.photos/200/300/?random" style="height:200px"/>
                <h4 style="min-height:45px;margin:5px 0 10px 0">
-                   {{$servicio[0]}}
+                   Nombre: {{$servicio['nombre']}}
+                   <br>
+                   Precio: {{$servicio['precio']}}
+                   <br>
+                   Categoría: {{$servicio['categoria']}}
+                   <br>
+                   Imagen: {{$servicio['imagen']}}
+                   <br>
+                   Descripción: {{$servicio['descripcion']}}
                </h4>
            </a>
 
