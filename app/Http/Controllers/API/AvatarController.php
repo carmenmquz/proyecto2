fad75472166161b29eab79031d558614dbab7941
Jorge <1833981@alu.murciaeduca.es>
2023-02-10 10:00:47 +0100

    Asociado avatares a usuarios

---
+++ app/Http/Controllers/API/AvatarController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use App\Models\User;
+
+class AvatarController extends Controller
+{
+    public function store(Request $request)
+    {
+        $user = $request->user();
+        $path = $request->file('avatar')->store('public/avatars');
+        $user->avatar = $path;
+        $user->save();
+        return response()->json($user);
+    }
+
+    public function getAvatar(Request $request)
+    {
+        $user = $request->user();
+        return Storage::get($user->avatar);
+    }
+
+    public function getUserAvatar($user_id)
+    {
+        $user = User::findOrFail($user_id);
+        return Storage::get($user->avatar);
+    }
+}
