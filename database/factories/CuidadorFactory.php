6528d2e9a6745ae6bd9271933c956a25a4bbd43d
carmen <carmenmquz@gmail.com>
2023-02-19 02:34:13 +0100

    Terminados Tutor, Menor y Cuidador

---
+++ database/factories/CuidadorFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CuidadorFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $this->faker = \Faker\Factory::create('es_ES');
+        return [
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'direction' => $this->faker->streetAddress(),
+            'email' => $this->faker->email(),
+            'tlf' => $this->faker->randomNumber(9, true),
+            'tipo' => 'Particular',
+            'valoration' => $this->faker->numberBetween(1, 5, true)
+        ];
+    }
+}
--- database/migrations/2022_12_15_170545_create_cuidadors_table.php
+++ database/migrations/2022_12_15_170545_create_cuidadors_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCuidadorTable extends Migration
+class CreateCuidadorsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,20 +13,21 @@ class CreateCuidadorTable extends Migration
      */
     public function up()
     {
-        Schema::create('cuidador', function (Blueprint $table) {
+        Schema::create('cuidadors', function (Blueprint $table) {
             $table->id();
             $table->string('first_name', 50);
             $table->string('last_name', 50)->nullable();
             $table->string('direction');
             $table->string('email')->unique();
             $table->string('tlf', 9)->unique();
+            $table->enum('tipo', ['Centro', 'Particular']);
             $table->integer('valoration');
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
 
-        DB::statement('ALTER TABLE cuidador ADD CONSTRAINT valoration_range CHECK (valoration BETWEEN 1 AND 5)');
+        DB::statement('ALTER TABLE cuidadors ADD CONSTRAINT valoration_range CHECK (valoration BETWEEN 1 AND 5)');
     }
 
     /**
@@ -36,6 +37,6 @@ class CreateCuidadorTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('cuidador');
+        Schema::dropIfExists('cuidadors');
     }
 }
